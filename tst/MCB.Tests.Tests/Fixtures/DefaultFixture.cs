8106fe045b6750146b4aee861c50b6ef0daf4f77
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-09-25 21:24:22 -0300

    refact: update MCB.Core.Infra.CrossCutting.DependencyInjection package

--- tst/MCB.Tests.Tests/Fixtures/DefaultFixture.cs
+++ tst/MCB.Tests.Tests/Fixtures/DefaultFixture.cs
@@ -3,7 +3,6 @@ using MCB.Core.Infra.CrossCutting.DependencyInjection.Abstractions.Interfaces;
 using MCB.Tests.Fixtures;
 using MCB.Tests.Tests.Services;
 using MCB.Tests.Tests.Services.Interfaces;
-using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace MCB.Tests.Tests.Fixtures;
@@ -17,17 +16,14 @@ public class DefaultFixtureCollection
 public class DefaultFixture
     : FixtureBase
 {
-    // Fields
-    private IServiceCollection _serviceCollection;
-
     // Protected Methods
     protected override IDependencyInjectionContainer CreateDependencyInjectionContainerInternal()
     {
-        return new DependencyInjectionContainer(new ServiceCollection());
+        return new DependencyInjectionContainer();
     }
     protected override void BuildDependencyInjectionContainerInternal(IDependencyInjectionContainer dependencyInjectionContainer)
     {
-        ((DependencyInjectionContainer)dependencyInjectionContainer).Build(_serviceCollection.BuildServiceProvider());
+        ((DependencyInjectionContainer)dependencyInjectionContainer).Build();
     }
     protected override void ConfigureDependencyInjectionContainerInternal(IDependencyInjectionContainer dependencyInjectionContainer)
     {
